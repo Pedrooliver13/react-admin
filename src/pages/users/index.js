6cd0e88c44491635e1c97cb21d4fb9cc37afac38
Pedro Oliveira | Stone <pedro.holiveira@stone.com.br>
2022-03-11 14:46:43 -0300

    feat: Using lib react-admin

---
+++ src/pages/users/index.js
@@ -0,0 +1,56 @@
+import { Grid } from "@material-ui/core";
+import {
+  Create,
+  Datagrid,
+  Edit,
+  EditButton,
+  List,
+  SimpleForm,
+  TextField,
+  TextInput,
+} from "react-admin";
+
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
+
+export const UserList = (props) => {
+  return (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" sortable={false} />
+        <EditButton basePath="/posts" />
+      </Datagrid>
+    </List>
+  );
+};
+
+export const UserCreate = (props) => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput source="name" />
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export const UserUpdate = (props) => {
+  return (
+    <Edit title={<PostTitle />} {...props}>
+      <SimpleForm>
+        <Grid container spacing={2}>
+          <Grid item md={3}>
+            <TextInput source="name" />
+          </Grid>
+          <Grid item md={6}>
+            <TextInput source="name" />
+          </Grid>
+        </Grid>
+      </SimpleForm>
+    </Edit>
+  );
+};
+
